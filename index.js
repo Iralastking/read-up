39ef23a9e9f89bf0c10e56c5eba1d73e1cbdecf2
jonathan bass <jonathanbass@yahoo.com>
2023-06-12 17:54:13 -0400

    changed some index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 // TODO: Create an array of questions for user input
 const questions = [];
-
+  
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
@@ -11,3 +11,17 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+if (index < questions.length - 1) {
+    askQuestion(index + 1);
+  } else {
+    console.log("Thank you for your answers:");
+    console.log(answers);
+  
+    const fileName = "README.md";
+    const data = generateReadmeContent(answers);
+    writeToFile(fileName, data);
+  
+    rl.close();
+  }
+  
